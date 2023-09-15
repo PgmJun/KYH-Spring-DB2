ae205449d5c4abb189ecf690442ebf39fa3aebc0
PgmJun <chltmdwns96@gmail.com>
2023-09-15 16:45:12 +0900

    데이터 접근 기술 - MyBatis 학습 완료
    [느낀점]
    Mybatis 사용을 통해 동적쿼리 작성의 복잡도를 굉장히 낮출 수 있었다.
    하지만 JPA와 같은 ORM기술과 비교했을 때, 직접 SQL을 작성하기 때문에 휴먼 에러의 여지가 남아있고, 복잡한 쿼리 작성도 가능하지만 깊이있게 알아야 가능한 것 같다. 배울 때마다 wow가 나오는 개발자가 성공한다고 하지만, 이미 JPA의 맛을 알아버렸기 때문에 wow가 나오지 않는다. 대신 배우면 배울 수록 JPA에 대해 WOW가 나오는 걸 참을 수가 없다. 나도 괜찮은 개발자가 되려나보다.

--- itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,20 +1,17 @@
 package hello.itemservice;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
